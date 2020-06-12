b0f2a0efc2004cf05933a1b46debe06e03bb9c84
YiYang-Clotho <s3798354@student.rmit.edu.au>
2020-06-13 06:33:49 +1000

    delete log

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,10 @@ class ApplicationController < ActionController::Base
     skip_before_action :verify_authenticity_token    
     def home
         @default_zone = Time.now.utc+10.hours
-        @session = session[:status]
     end
 
     def set_status
         session[:status] = params[:status]
-        logger = Logger.new(STDOUT)
-        logger.info(params[:status])
-        logger.info(session[:status])
     end
     
     include ApplicationHelper
